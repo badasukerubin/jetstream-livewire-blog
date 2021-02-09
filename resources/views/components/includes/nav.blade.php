96f5d9773e4ceab21d03dcf1917bdc20b2f4d8f3
badasukerubin <kelvinvip8@gmail.com>
2021-02-09 18:00:02 +0100

    WIP: Dashboard CRUD UI

--- resources/views/components/includes/nav.blade.php
+++ resources/views/components/includes/nav.blade.php
@@ -8,8 +8,12 @@
     <div :class="open ? 'block': 'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
         <div
             class="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
+            @auth
+            <a href="{{route('dashboard')}}" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Dashboard</a>
+            @else
             <a href="{{route('login')}}" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Login</a>
             <a href="{{route('register')}}" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Register</a>
+            @endauth
         </div>
     </div>
 </nav>
